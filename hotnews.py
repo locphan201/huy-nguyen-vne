53e9756284630c92935a6fadf01e66b5a9ece46a
locphan201 <marcofe911@gmail.com>
2024-06-14 16:12:00 +0700

    first commit

---
+++ hotnews.py
@@ -0,0 +1,30 @@
+import requests
+from tqdm import tqdm
+
+links = [f'https://vnexpress.net/microservice/listhotnews/type/{i}' for i in range(1, 9)]
+
+urls = []
+
+for link in tqdm(links, desc='Process pages'):
+    response = requests.get(link)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        if 'message' in data:
+            tokens = data['message'].split(' ')
+            for token in tokens:
+                if not 'https://vnexpress.net' in token:
+                    continue
+                url = token.replace('href="', '').replace('"', '')
+                if not url in urls and not '#box_comment_vne' in token:
+                    urls.append(url)
+        else:
+            print('Empty message')
+    else:
+        print(f'Failed to retrieve the web page. Status code: {response.status_code}')
+
+with open('hot_news_articles.txt', 'w', encoding='utf-8') as file:
+    file.write('\n'.join(urls))
+    
+print(f'Total urls: {len(urls)}')
---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import sys
+import os
+
+FILEPATH = ''
+
+try:
+    with open('hot_news_articles.txt', 'r') as file:
+        urls = file.readlines()
+except Exception as e:
+    print('Error:', str(e))
+    sys.exit()
+
+if len(urls) == 0:
+    print('Please run hotnews.py first!')
+    sys.exit()
+
+if not os.path.exists(FILEPATH):
+    with open(FILEPATH, 'w', encoding='utf-8') as file:
+        file.write('')
+
+urls = [url.replace('\n', '') for url in urls]
+
+for url in tqdm(urls, desc='Retrieving content'):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        article_detail = soup.find('article', class_='fck_detail')
+
+        if article_detail:
+            content = '\n'.join([p.get_text(strip=True) for p in article_detail.find_all('p')])
+
+            with open(FILEPATH, 'a', encoding='utf-8') as file:
+                file.write(content + '\n\n')
+    else:
+        print(f'Failed to retrieve the web page. Status code: {response.status_code}')
